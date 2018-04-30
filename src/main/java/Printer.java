559c489f32de4d71930088b83383de222375ccba
Paul Kelly <paul.kelly20@gmail.com>
2018-04-30 20:53:01 +0100

    Printer class working

--- src/main/java/Printer.java
+++ src/main/java/Printer.java
@@ -1,2 +1,51 @@
 public class Printer {
+
+    private int paper;
+    private int toner;
+
+    public Printer(int paper, int toner){
+        this.paper = paper;
+        this.toner = toner;
+    }
+
+    public int getPaper() {
+        return this.paper;
+    }
+
+
+    public int getToner() {
+        return this.toner;
+    }
+
+    public int paperNeeded(int pages, int copies) {
+        return pages * copies;
+    }
+
+    public boolean enoughPaperInPrinter(int pages_needed) {
+        if (this.paper > pages_needed)
+        {return true;} else {return false;}
+    }
+
+    public boolean enoughTonerInPrinter(int pages_needed){
+        if (this.toner > pages_needed)
+        {return true;} else {return false;}
+    }
+
+    public void print(int pages_needed){
+        this.paper -= pages_needed;
+        this.toner -= pages_needed;
+
+    }
+
+    public void checkIfCanPrint(int pages_needed){
+        if (enoughPaperInPrinter(pages_needed) && enoughTonerInPrinter(pages_needed))
+        {print(pages_needed);}
+
+    }
+
+    public int fillPaper(){
+        return this.paper = 100;
+    }
+
+
 }
--- src/test/java/PrinterTest.java
+++ src/test/java/PrinterTest.java
@@ -1,2 +1,73 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+
 public class PrinterTest {
+
+    private Printer testprinter;
+
+    @Before
+    public void before(){
+        testprinter = new Printer(50, 100);
+    }
+
+    @Test
+    public void testPaperAmount() {
+        assertEquals(50, testprinter.getPaper());
+    }
+
+    @Test
+    public void testTonerAmount() {
+        assertEquals(100, testprinter.getToner());
+    }
+
+    @Test
+    public void testAmountOfPaperNeededPerPrintJob() {
+        assertEquals(20, testprinter.paperNeeded(4, 5));
+    }
+
+    @Test
+    public void testEnoughPaperInPrinterReturnsTrue() {
+        assertEquals(true, testprinter.enoughPaperInPrinter(testprinter.paperNeeded(4,5)));
+    }
+
+
+    @Test
+    public void testEnoughPaperInPrinterReturnsfalse() {
+        assertEquals(false, testprinter.enoughPaperInPrinter(testprinter.paperNeeded(10,6)));
+    }
+
+    @Test
+    public void testPaperAfterPrintJob(){
+        testprinter.checkIfCanPrint(testprinter.paperNeeded(4,5));
+
+        assertEquals(30, testprinter.getPaper());
+    }
+
+    @Test
+    public void testPaperHasBeenRefilled(){
+        testprinter.fillPaper();
+        assertEquals(100, testprinter.getPaper());
+    }
+
+    @Test
+    public void testEnoughTonerToPrintReturnsFalse(){
+        assertEquals(true, testprinter.enoughTonerInPrinter(testprinter.paperNeeded(10,6)));
+    }
+
+    @Test
+    public void testEnoughTonerToPrintReturnsTrue(){
+        assertEquals(false, testprinter.enoughTonerInPrinter(testprinter.paperNeeded(100,6)));
+    }
+
+    @Test
+    public void testTonerAfterPrintJob(){
+        testprinter.checkIfCanPrint(testprinter.paperNeeded(4,5));
+        assertEquals(80, testprinter.getToner());
+    }
+
 }
+
+
